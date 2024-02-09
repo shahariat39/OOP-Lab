8caa1883e97bc73e0fd6658e9211a9b108e80307
shahariat39 <117737270+shahariat39@users.noreply.github.com>
2024-02-09 18:20:51 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,60 +1,39 @@
 
 public class Main {
     public static void main(String[] args) {
-        class Bakery {
-            private int breadCount;
-            private final Lock lock = new ReentrantLock();
-            private final Condition breadAvailable = lock.newCondition();
+        class Philosopher extends Thread {
+            private final int philosopherNumber;
+            private final Object leftFork;
+            private final Object rightFork;
 
-            public Bakery() {
-                this.breadCount = 0;
-            }
-
-            public void produceBread() throws InterruptedException {
-                lock.lock();
-                try {
-                    breadCount++;
-                    System.out.println("Bakery produced 1 bread. Total bread count: " + breadCount);
-                    breadAvailable.signalAll();
-                } finally {
-                    lock.unlock();
-                }
-                Thread.sleep(3000); // Producing bread takes 3 seconds
-            }
-
-            public boolean sellBread() throws InterruptedException {
-                lock.lock();
-                try {
-                    if (breadCount > 0) {
-                        breadCount--;
-                        System.out.println("Shop bought 1 bread. Remaining bread count: " + breadCount);
-                        return true;
-                    } else {
-                        System.out.println("No bread available. Waiting for bakery to produce bread...");
-                        breadAvailable.await();
-                        return false;
-                    }
-                } finally {
-                    lock.unlock();
-                }
-            }
-        }
-
-        class Shop extends Thread {
-            private final Bakery bakery;
-
-            public Shop(Bakery bakery) {
-                this.bakery = bakery;
+            public Philosopher(int philosopherNumber, Object leftFork, Object rightFork) {
+                this.philosopherNumber = philosopherNumber;
+                this.leftFork = leftFork;
+                this.rightFork = rightFork;
             }
 
             @Override
             public void run() {
                 try {
                     while (true) {
-                        if (bakery.sellBread()) {
-                            // Customer bought bread
-                            Thread.sleep(1000); // Simulating customer enjoying the bread
+                        // Thinking
+                        System.out.println("Philosopher " + philosopherNumber + " is thinking.");
+
+                        synchronized (leftFork) {
+                            System.out.println("Philosopher " + philosopherNumber + " picked up left fork " + philosopherNumber);
+                            synchronized (rightFork) {
+                                // Eating
+                                System.out.println("Philosopher " + philosopherNumber + " is eating spaghetti.");
+
+                                // Simulating eating time
+                                Thread.sleep(1000);
+                            }
+                            System.out.println("Philosopher " + philosopherNumber + " put down right fork " + ((philosopherNumber + 1) % 3));
                         }
+                        System.out.println("Philosopher " + philosopherNumber + " put down left fork " + philosopherNumber);
+
+                        // Simulating thinking time
+                        Thread.sleep(1000);
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -64,24 +43,18 @@ public class Main {
 
         public class Main {
             public static void main(String[] args) {
-                Bakery bakery = new Bakery();
-                int numOfShops = 3; // Number of shop threads
-
-                // Create and start shop threads
-                for (int i = 0; i < numOfShops; i++) {
-                    new Shop(bakery).start();
+                // Create forks
+                Object[] forks = new Object[3];
+                for (int i = 0; i < 3; i++) {
+                    forks[i] = new Object();
                 }
 
-                // Start bakery thread
-                new Thread(() -> {
-                    try {
-                        while (true) {
-                            bakery.produceBread();
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }).start();
+                // Create philosophers
+                Philosopher[] philosophers = new Philosopher[3];
+                for (int i = 0; i < 3; i++) {
+                    philosophers[i] = new Philosopher(i, forks[i], forks[(i + 1) % 3]);
+                    philosophers[i].start();
+                }
             }
         }
     }
